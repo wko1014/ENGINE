8f723d66c2e8ff366a6c83630642b836dfc9662a
Wonjun Ko <wjko@korea.ac.kr>
2022-02-22 14:02:53 +0900

    Update engine.py

--- engine.py
+++ engine.py
@@ -76,19 +76,23 @@ class engine(tf.keras.Model):
         )
 
     @tf.function
+    # Reconstructed SNPs sampling
     def sample(self, eps=None):
         if eps is None:
             eps = tf.random.normal(shape=(10, 50))
         return self.decode(eps, apply_sigmoid=True)
 
+    # Represent mu and sigma from the input SNP
     def encode(self, x_SNP):
         mean, logvar = tf.split(self.encoder(x_SNP), num_or_size_splits=2, axis=1)
         return mean, logvar
 
+    # Construct latent distribution
     def reparameterize(self, mean, logvar):
         eps = tf.random.normal(shape=mean.shape)
         return eps * tf.math.exp(logvar * .5) + mean
 
+    # Reconstruct the input SNP
     def decode(self, z_SNP, apply_sigmoid=False):
         logits = self.decoder(z_SNP)
         if apply_sigmoid:
@@ -96,14 +100,17 @@ class engine(tf.keras.Model):
             return probs
         return logits
 
+    # Attentive vector and fake neuroimaging generation
     def generate(self, z_SNP, c_demo):
         z = tf.concat([c_demo, z_SNP], axis=-1)
         x_MRI_fake, a = tf.split(self.generator(z), num_or_size_splits=2, axis=1)
         return x_MRI_fake, a
 
+    # Classify the real and the fake neuroimaging
     def discriminate(self, x_MRI_real_or_fake):
         return self.discriminator(x_MRI_real_or_fake)
 
+    # Downstream tasks; brain disease diagnosis and cognitive score prediction
     def diagnose(self, x_MRI, a, apply_logistic_activation=False):
         feature = self.diagnostician_share(tf.multiply(x_MRI, a)) # Hadamard production of the attentive vector
         logit_clf = self.diagnostician_clf(feature)
